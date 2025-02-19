0e0eb1ad1533b64cd4e6fb52771ba502235dba71
Muhammad Omar Farooq <mohamed.omar.farooq@gmail.com>
2025-02-19 06:01:24 +0500

    started solving remove element problem

---
+++ Neetcode250/arrays_hashing/RemoveElement.py
@@ -0,0 +1,57 @@
+import unittest
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        nums.remove(val)
+        k = len(nums)
+        
+        return k
+
+
+class TestRemoveElement(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_case_1(self):
+        nums = [3, 2, 2, 3]
+        val = 3
+        expectedNums = [2, 2]  # Order can be different
+        k = self.solution.removeElement(nums, val)
+        self.assertEqual(k, len(expectedNums))
+        self.assertCountEqual(nums[:k], expectedNums)  # Check unordered match
+
+    def test_case_2(self):
+        nums = [0, 1, 2, 2, 3, 0, 4, 2]
+        val = 2
+        expectedNums = [0, 1, 3, 0, 4]
+        k = self.solution.removeElement(nums, val)
+        self.assertEqual(k, len(expectedNums))
+        self.assertCountEqual(nums[:k], expectedNums)
+
+    def test_case_3(self):
+        nums = [1, 1, 1, 1, 1]
+        val = 1
+        expectedNums = []
+        k = self.solution.removeElement(nums, val)
+        self.assertEqual(k, len(expectedNums))
+        self.assertCountEqual(nums[:k], expectedNums)
+
+    def test_case_4(self):
+        nums = [4, 5, 6, 7, 8]
+        val = 3
+        expectedNums = [4, 5, 6, 7, 8]
+        k = self.solution.removeElement(nums, val)
+        self.assertEqual(k, len(expectedNums))
+        self.assertCountEqual(nums[:k], expectedNums)
+
+    def test_case_5(self):
+        nums = []
+        val = 1
+        expectedNums = []
+        k = self.solution.removeElement(nums, val)
+        self.assertEqual(k, len(expectedNums))
+        self.assertCountEqual(nums[:k], expectedNums)
+
+if __name__ == '__main__':
+    unittest.main()
+
